03a89bd150a9c8fc3031aecb76d2b27446683fc4
CrocdileChan <CrocdileChan@github.com>
2019-09-06 00:02:17 +0800

    debug

--- runtime/src/data_space.rs
+++ runtime/src/data_space.rs
@@ -1,14 +1,10 @@
-#[cfg(feature = "hyper")]
-extern crate hyper;
 
-use ipfs_api::IpfsClient;
+use rstd::vec::Vec;
 use parity_codec::{Decode, Encode};
 use runtime_primitives::traits::{As, Hash};
 use support::{
     decl_event, decl_module, decl_storage, dispatch::Result, ensure, StorageMap, StorageValue,traits::Currency};
 use system::ensure_signed;
-use std::io::Cursor;
-use futures::future::Future;
 
 pub trait Trait: balances::Trait {
     type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
@@ -16,8 +12,8 @@ pub trait Trait: balances::Trait {
 
 #[derive(Encode, Decode, Default, Clone, PartialEq)]
 #[cfg_attr(feature = "std", derive(Debug))]
-pub struct DataSpace<Hash,Balance> {
-    user_id: Hash,
+pub struct DataSpace<AccountId,Hash,Balance> {
+    user_id: AccountId,
     user_name: Vec<u8>,
     // hashmap key is ipfs_hash
     data_to_price: Vec<Data<Hash,Balance>>,
@@ -29,9 +25,9 @@ pub struct DataSpace<Hash,Balance> {
 #[cfg_attr(feature = "std", derive(Debug))]
 pub struct Data<Hash,Balance>{
     id: Hash,
-    name: String,
+    name: Vec<u8>,
     owner: Hash,
-    ipfs_key: String,
+    ipfs_key: Vec<u8>,
     content: Vec<u8>,
     price:Balance,
 }
@@ -51,10 +47,12 @@ decl_event! {
 
 decl_storage! {
     trait Store for Module<T: Trait> as DataStore {
-        Owners get(owner_of): map T::AccountId => DataSpace<T::Hash,T::Balance>;
+        Owners get(owner_of): map T::AccountId => DataSpace<T::AccountId,T::Hash,T::Balance>;
 
         // data id => data struct
         Data_info get(data): map T::Hash => Data<T::Hash,T::Balance>;
+
+        //Nonce: u64;
     }
 }
 
@@ -62,15 +60,21 @@ decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
         fn deposit_event<T>() = default;
 
-        fn register_account(origin,user_name: Vec<u8>) -> Result {
-            <Owners<T>>::insert(origin,DataSpace{
-                user_id: origin,
+        fn register_account(origin, user_name: Vec<u8>) -> Result {
+            let user_id = ensure_signed(origin)?;
+            let new_data_space = DataSpace{
+                user_id: &user_id,
                 user_name: user_name,
                 data_to_price: Vec::new(),
                 bought_list: Vec::new(),
                 sold_list: Vec::new(),
-            });
-            Self::deposit_event(RawEvent::Registered(origin));
+            };
+            <Owners<T>>::insert(&user_id,new_data_space);
+
+            Self::deposit_event(RawEvent::Registered(user_id));
+
+            //<Nonce<T>>::mutate(|n| *n += 1);
+
             Ok(())
         }
 
@@ -84,23 +88,23 @@ decl_module! {
             let buyer = ensure_signed(origin)?;
             ensure!(buyer != seller, "you can't buy your own data");
 
-            let data_info = Self::data(data_id).ok_or("no data for sell")?;
+            let data_info = Self::data(data_id);//.ok_or("no data for sell")?;
             let data_price = data_info.price;
             ensure!(data_price <= pay_price, "the data you want to buy costs more than your pay");
 
             <balances::Module<T> as Currency<_>>::transfer(&buyer, &seller, data_price)?;
 
-            let mut seller_ds = Self::owner_of(&seller).ok_or("no seller account you want")?;
-            let mut buyer_ds = Self::owner_of(&buyer).ok_or("no account registered to buy data")?;
+            let mut seller_ds = Self::owner_of(&seller);//.ok_or("no seller account you want")?;
+            let mut buyer_ds = Self::owner_of(&buyer);//.or_not("no account registered to buy data")?;
             buyer_ds.bought_list.push(data_id);
             seller_ds.bought_list.push(data_id);
 
-            Self::deposit_event(RawEvent::Bought(buyer,seller,data_price));
+            Self::deposit_event(RawEvent::Bought(buyer,seller,data_id,data_price));
             Ok(())
         }
 
-         fn upload_to_space(origin,data_name: String,data: Vec<u8>) -> Result {
-            Self::write_to_ipfs(data);
+         fn upload_to_space(origin,data_name: Vec<u8>,data: Vec<u8>) -> Result {
+            //Self::write_to_ipfs(data);
             Ok(())
         }
 
@@ -120,22 +124,24 @@ decl_module! {
 
 impl<T: Trait> Module<T> {
 
-    fn write_to_ipfs(data: Vec<u8>) -> String {
-        let mut ipfs_hash: String = "".to_string();
-        let client: IpfsClient = IpfsClient::default();
-        let req = client
-            .add(Cursor::new(data)).map(|resp| ipfs_hash = resp.hash);
+    /*fn write_to_ipfs(data: Vec<u8>) -> Vec<u8> {
+        //let mut ipfs_hash: Vec<u8> = "".to_Vec<u8>();
+        //let client: IpfsClient = IpfsClient::default();
+        // let req = client
+        //   .add(Cursor::new(data)).map(|resp| ipfs_hash = resp.hash);
 
-        #[cfg(feature = "hyper")]
-        hyper::rt::run(req);
-        ipfs_hash
+        //#[cfg(feature = "hyper")]
+        //hyper::rt::run(req);
+        //ipfs_hash
+
     }
 
-    fn read_from_ipfs(hash: String) -> String {
-        let mut data: String = "".to_string();
-        let client: IpfsClient = IpfsClient::default();
-        let req = client.object_get(&hash).map(|resp| data=resp.data);
+    fn read_from_ipfs(hash: Vec<u8>) -> Vec<u8> {
+        let mut data: Vec<u8> = "".to_Vec<u8>();
+        //let client: IpfsClient = IpfsClient::default();
+        //let req = client.object_get(&hash).map(|resp| data=resp.data);
         data
     }
+    */
 
 }
